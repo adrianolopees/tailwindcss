a0418214a050730ee2bfe8bfaa1c31023329b590
adrianolopees <adrianodeev1@gmail.com>
2025-04-03 14:21:55 -0300

    parte do menu mobile finalizado

--- figma/homepage/src/js/main.js
+++ figma/homepage/src/js/main.js
@@ -33,3 +33,33 @@ txtEfeitos.forEach((txtEfeito) => {
     }, 40);
   });
 });
+
+const btnBurguer = document.getElementById("btnBurguer");
+const icon = btnBurguer.querySelector("i");
+const overlay = document.getElementById("overlay");
+const navBar = document.getElementById("nav");
+
+function menuBurguer() {
+  overlay.classList.toggle("invisible");
+  overlay.classList.toggle("opacity-0");
+  navBar.classList.toggle("hidden");
+
+  if (icon.classList.contains("bi-list")) {
+    icon.classList.remove("bi-list");
+    icon.classList.add("bi-x");
+  } else {
+    icon.classList.remove("bi-x");
+    icon.classList.add("bi-list");
+  }
+}
+
+btnBurguer.addEventListener("click", menuBurguer);
+
+overlay.addEventListener("click", () => {
+  overlay.classList.add("invisible");
+  overlay.classList.remove("opacity-50");
+  navBar.classList.add("hidden");
+
+  icon.classList.remove("bi-x");
+  icon.classList.add("bi-list");
+});
