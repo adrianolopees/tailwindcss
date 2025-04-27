cec5bef8bebd9a7e9395cb2da6fb0a882521f657
adrianolopees <adrianodeev1@gmail.com>
2025-04-27 15:23:47 -0300

    pequenos ajustes no footer

---
+++ figma/coinfusion/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./**/*.html",
+    "./src/**/*.js",
+    "./node_modules/bootstrap-icons/**/*.css",
+  ], // Verifica os arquivos dentro de `src/`
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
